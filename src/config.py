ae3743ad011e5eedb7961c40d5fe6ad39870e0db
ChainHokes <k.nazar2003@gmail.com>
2022-07-22 16:04:46 +0300

    docker added

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
-    port: int = 8080
-    host: str = '127.0.0.1'
+    port: int = 8000
+    host: str = '0.0.0.0'
     reload: bool = True
 
 config = Config()
